41f7e6b8e5ecca98c102a44399e44709e71c7bcc
Ilya <ykin100500@gmail.com>
2023-04-16 15:41:32 +0400

    3.3

--- task_2/solution.js
+++ task_2/solution.js
@@ -11,7 +11,7 @@ function calcShipping(sum, min, shipping) {
     if ( productsSum >= freeShippingMinSum) {
         shippingSum = 0;
     } 
-    else if ((productsSum > 0) & (productsSum < freeShippingMinSum)) {
+    else if ((productsSum > 0) && (productsSum < freeShippingMinSum)) {
     shippingSum = shippingPrice;
     }
 
